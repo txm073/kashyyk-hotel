8c1382bd657aa22d6730c66223e8e4d306091a5f
Tom Barnes <tomtombarnes2@gmail.com>
2022-11-04 09:45:41 +0000

    created function prototypes and main interface

--- main.c
+++ main.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Booking {
+typedef struct {
     char* firstName, lastName, dob, id;
     int nDays, nAdults, nChildren, paper, roomNum;
     char boardType;
-};
+} Booking;
+
+void checkIn(Booking* booking)
+{
+
+}
+
+void checkOut(Booking booking)
+{
+
+}
+
+void bookTable(Booking booking)
+{
+
+}
 
 int main(const int argc, const char** argv) 
 {
-    printf("Hello World!\n");
+    char option[32];
+    printf("Choose an action: ");
+    scanf("%s", &option);
+    Booking booking;
+        
+    if (strcmp((const char*)option, "check in") == 0) {
+        checkIn(&booking);
 
+    } else if (strcmp((const char*)option, "check out") == 0) {
+        checkOut(booking);
+
+    } else if (strcmp((const char*)option, "book table") == 0) {
+        bookTable(booking);    
+
+    }
+    
     return 0;
 }
