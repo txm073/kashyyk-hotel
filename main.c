c975318f374c9155820f7f28e8e716810bfb359a
Tom Barnes <tomtombarnes2@gmail.com>
2022-11-04 09:25:07 +0000

    Files updated at 04/11/2022 - 09:25

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Booking {
+    char* firstName, lastName, dob, id;
+    int nDays, nAdults, nChildren, paper, roomNum;
+    char boardType;
+};
+
 int main(const int argc, const char** argv) 
 {
     printf("Hello World!\n");
