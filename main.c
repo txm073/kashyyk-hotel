bdd228acfb1b02f4ccd2bc117010399d4df31f3f
Mikhail <mikhail.gray22@students.bhasvic.ac.uk>
2022-11-14 14:58:19 +0000

    Add date

--- main.c
+++ main.c
@@ -240,7 +240,17 @@ void checkIn()
 // Check out function (Mikhail)
 void checkOut()
 {
-    printf("checking out...\n");
+    // Get current year
+    time_t s, val = 1;
+    struct tm* current_time;
+    s = time(NULL);
+    current_time = localtime(&s);
+    printf("Date of bill %d.%d.%d\n",
+           current_time->tm_mday,
+           current_time->tm_mon + 1,
+           current_time->tm_year + 1900);
+}
+
 }
 
 // Table booking function (Tom)
