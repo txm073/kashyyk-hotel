204d423fb1b08504f4fd17c6de47254f29c2001b
Mikhail <mikhail.gray22@students.bhasvic.ac.uk>
2022-11-16 11:39:38 +0000

    +++

--- main.c
+++ main.c
@@ -352,7 +352,7 @@ void checkIn()
    }
 
    if(roomChoice == 2 && room2Available == 1)
-   {
+   {f
     room2Available == 0;
     printf("You have booked room 2\n__________________________\n");
    }
@@ -396,15 +396,35 @@ void checkOut()
 
     // Booking ID
     char bokid[10];
+    int roomnum = 0,total = 0,year = 0;
     printf("Enter your booking ID:");
     scanf("%s", bokid);
+    for (i >= 6; i = 0; i++){
+        if booking[i].id == bokid{
+            printf("Valid booking ID")
+            roomnum = i;
+
+        }
+    }
+    year = booking[roomnum].dob % 1000;
+
+    printf("Date of bill %d.%d.%d\n",
+           current_time->tm_mday,
+           current_time->tm_mon + 1,
+           current_time->tm_year + 1900);
+    printf("Booking ID: %s", bokid);
+    printf("Main user: %s %s", booking[roomnum].firstname, booking[roomnum].lastname)
+    printf("--------------------");
+    if year
+        printf("Room price: %d",prices[roomnum])
+
+    if booking[roomnum].paper == TRUE{
+        printf("Newspaper: 5.50")
+        total = total + 5.50
+    }
+    printf("Total price: %0.2f", total)
 
 
-    // time of bill
-    time_t s, val = 1;
-    time_t s, val = 1;
-    struct tm* current_time;
-    s = time();
 }
 
 // Table booking function (Tom)
